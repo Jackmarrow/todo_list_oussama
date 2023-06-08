b7ce52226ed82e52b834291707a0cecf503058fe
Oussama Bof <jackmarrow06@gmail.com>
2023-06-08 17:55:07 +0200

    Delete & modify & check functionality added

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,27 @@ const columnContainer = document.getElementById("columnContainer");
 
 addColumnBtn.addEventListener("click", addDiv);
 
+// an event listnener that listen to the click and add a task
+document.addEventListener("click", e =>{
+  // check if the clicked button is an add task
+  if(e.target.className.includes("add-task")){
+      addTask(e.target);
+  }
+  //delete the task from the column
+  if(e.target.className.includes("delete")){
+    deleteTask(e.target);
+  }
+  //make the task on mode done
+  if(e.target.className.includes("done")){
+    done(e.target);
+  }
+  //modifiy the task
+  if(e.target.className.includes("modify")){
+    modify(e.target);
+  }
+}
+)
+
 //!------------------------------------------------------------------
 
 function addDiv() {
@@ -37,22 +58,12 @@ function addDiv() {
   }
 }
 
-// an event listnener that listen to the click and add a task
-document.addEventListener("click", e =>{
-  // check if the clicked button is an add task
-  if(e.target.className.includes("add-task")){
-      addTask(e.target);
-  }
-
-}
-)
-
 // addtask function
 function addTask(btn){
   let parentDiv = btn.parentElement;
   //create tags for the column
   let divTask = document.createElement("div");
-  let h5 = document.createElement("h5");
+  let p = document.createElement("p");
   let divButton = document.createElement("div");
   let btnModifie = document.createElement("button");
   let btnDelete = document.createElement("button");
@@ -61,19 +72,20 @@ function addTask(btn){
   let deleteIcon = document.createElement("i");
   let doneIcon = document.createElement("i");
   //add class to tags
-  divTask.setAttribute("class", "d-flex align-items-center justify-content-between task rounded-3 px-2");
+  divTask.setAttribute("class", "d-flex align-items-center justify-content-between task rounded-3 p-2");
   divButton.setAttribute("class", "d-flex align-content-center");
-  btnModifie.setAttribute("class", "mx-1 bg-transparent border-0 color-blue modify");
-  btnDelete.setAttribute("class", "mx-1 bg-transparent border-0 color-red delete");
-  btnDone.setAttribute("class","mx-1 p-1 rounded-2 border-0 bg-success text-white done");
-  modifieIcon.setAttribute("class","fa-solid fa-pencil fa-2x");
-  deleteIcon.setAttribute("class","fa-solid fa-trash fa-2x");
-  doneIcon.setAttribute("class", "fa-solid fa-check text-white px-2");
+  btnModifie.setAttribute("class", "mx-1 bg-transparent border-0 color-blue");
+  btnDelete.setAttribute("class", "mx-1 bg-transparent border-0 color-red");
+  btnDone.setAttribute("class","mx-1 p-1 rounded-2 border-0 bg-success text-white");
+  modifieIcon.setAttribute("class","fa-solid fa-pencil fa-2x modify");
+  deleteIcon.setAttribute("class","fa-solid fa-trash fa-2x delete");
+  doneIcon.setAttribute("class", "fa-solid fa-check text-white px-1 done");
+  p.setAttribute("class","fs-5 mb-0")
   //append child to the column
   //^ add div task to the column
   parentDiv.appendChild(divTask);
-  //^ add h5 to div task 
-  divTask.appendChild(h5);
+  //^ add p to div task 
+  divTask.appendChild(p);
   //^ add all div button to div task 
   divTask.appendChild(divButton);
   //^ add btn modifie to div buttons
@@ -86,5 +98,25 @@ function addTask(btn){
   divButton.appendChild(btnDone);
   btnDone.appendChild(doneIcon);
   // create content of button
-  h5.textContent = prompt("Saisir ton task ici:");
+  p.textContent = prompt("Saisir ton task ici:");
+}
+
+
+//delete task function 
+function deleteTask(deleteBtn){
+    let taskDiv = deleteBtn.parentElement.parentElement.parentElement;
+    taskDiv.remove();
+}
+
+//task done function 
+function done(doneBtn){
+  let taskDiv = doneBtn.parentElement.parentElement.parentElement;
+  taskDiv.classList.toggle("bg-green");
+}
+
+//task modify function
+function modify(modifyBtn){
+  let newTask = prompt("Ajouter ton modification?");
+  let p = modifyBtn.parentElement.parentElement.parentElement.firstChild;
+  p.textContent = newTask;
 }
