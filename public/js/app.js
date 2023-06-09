4d545bd42c696f8006fec8fec25134038ba86753
Oussama Bof <jackmarrow06@gmail.com>
2023-06-09 10:04:54 +0200

    Drag and drop functionality added

--- public/js/app.js
+++ public/js/app.js
@@ -2,35 +2,40 @@
 const inputTitle = document.getElementById("columnTitle");
 const addColumnBtn = document.getElementById("addColumn");
 const columnContainer = document.getElementById("columnContainer");
-
+let dragged = null;
 //!------------------------------------------------------------------
 
 addColumnBtn.addEventListener("click", addDiv);
+inputTitle.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    addDiv();
+  }
+});
 
 // an event listnener that listen to the click and add a task
-document.addEventListener("click", e =>{
+document.addEventListener("click", (e) => {
   // check if the clicked button is an add task
-  if(e.target.className.includes("add-task")){
-      addTask(e.target);
+  if (e.target.className.includes("add-task")) {
+    addTask(e.target);
   }
   //delete the task from the column
-  if(e.target.className.includes("delete")){
+  if (e.target.className.includes("delete")) {
     deleteTask(e.target);
   }
   //make the task on mode done
-  if(e.target.className.includes("done")){
+  if (e.target.className.includes("done")) {
     done(e.target);
   }
   //modifiy the task
-  if(e.target.className.includes("modify")){
+  if (e.target.className.includes("modify")) {
     modify(e.target);
   }
-}
-)
+});
 
 //!------------------------------------------------------------------
 
-function addDiv() {
+function addDiv(e) {
+  inputTitle.value = inputTitle.value.trim();
   // check if the input value is different than 0
   if (inputTitle.value.length != 0) {
     //create tags for the column
@@ -40,7 +45,7 @@ function addDiv() {
     //add class to tags
     div.setAttribute(
       "class",
-      "bg-danger h-100 task-width rounded-3 d-flex flex-column align-items-center ms-4 me-4 p-4 box-container"
+      "bg-danger h-100 task-width rounded-3 d-flex flex-column align-items-center ms-4 me-4 p-4 dropzone"
     );
     button.setAttribute("class", "btn btn-primary mb-4 add-task");
     h1.setAttribute("class", "text-white mb-4");
@@ -53,13 +58,13 @@ function addDiv() {
     div.appendChild(button);
     //reset the input value
     inputTitle.value = "";
-    //call the add task event listener function
-
+    //call the drag event listener
+    draggEvent();
   }
 }
 
 // addtask function
-function addTask(btn){
+function addTask(btn) {
   let parentDiv = btn.parentElement;
   //create tags for the column
   let divTask = document.createElement("div");
@@ -72,21 +77,28 @@ function addTask(btn){
   let deleteIcon = document.createElement("i");
   let doneIcon = document.createElement("i");
   //add class to tags
-  divTask.setAttribute("class", "d-flex align-items-center justify-content-between task rounded-3 p-2");
+  divTask.setAttribute(
+    "class",
+    "d-flex align-items-center justify-content-between task rounded-3 p-2"
+  );
+  divTask.setAttribute("draggable", "true");
   divButton.setAttribute("class", "d-flex align-content-center");
   btnModifie.setAttribute("class", "mx-1 bg-transparent border-0 color-blue");
   btnDelete.setAttribute("class", "mx-1 bg-transparent border-0 color-red");
-  btnDone.setAttribute("class","mx-1 p-1 rounded-2 border-0 bg-success text-white");
-  modifieIcon.setAttribute("class","fa-solid fa-pencil fa-2x modify");
-  deleteIcon.setAttribute("class","fa-solid fa-trash fa-2x delete");
+  btnDone.setAttribute(
+    "class",
+    "mx-1 p-1 rounded-2 border-0 bg-success text-white"
+  );
+  modifieIcon.setAttribute("class", "fa-solid fa-pencil fa-2x modify");
+  deleteIcon.setAttribute("class", "fa-solid fa-trash fa-2x delete");
   doneIcon.setAttribute("class", "fa-solid fa-check text-white px-1 done");
-  p.setAttribute("class","fs-5 mb-0")
+  p.setAttribute("class", "fs-5 mb-0");
   //append child to the column
   //^ add div task to the column
   parentDiv.appendChild(divTask);
-  //^ add p to div task 
+  //^ add p to div task
   divTask.appendChild(p);
-  //^ add all div button to div task 
+  //^ add all div button to div task
   divTask.appendChild(divButton);
   //^ add btn modifie to div buttons
   divButton.appendChild(btnModifie);
@@ -98,25 +110,54 @@ function addTask(btn){
   divButton.appendChild(btnDone);
   btnDone.appendChild(doneIcon);
   // create content of button
-  p.textContent = prompt("Saisir ton task ici:");
+  let taskText = prompt("Saisir ton task ici:");
+  while (taskText.length == 0) {
+    taskText = prompt("Saisir ton task ici:");
+  }
+  p.textContent = taskText;
 }
 
-
-//delete task function 
-function deleteTask(deleteBtn){
-    let taskDiv = deleteBtn.parentElement.parentElement.parentElement;
-    taskDiv.remove();
+//delete task function
+function deleteTask(deleteBtn) {
+  let taskDiv = deleteBtn.parentElement.parentElement.parentElement;
+  taskDiv.remove();
 }
 
-//task done function 
-function done(doneBtn){
+//task done function
+function done(doneBtn) {
   let taskDiv = doneBtn.parentElement.parentElement.parentElement;
   taskDiv.classList.toggle("bg-green");
 }
 
 //task modify function
-function modify(modifyBtn){
-  let newTask = prompt("Ajouter ton modification?");
+function modify(modifyBtn) {
+  let newTask = prompt("Ajouter ton modification ici?");
   let p = modifyBtn.parentElement.parentElement.parentElement.firstChild;
   p.textContent = newTask;
+}
+
+//
+function draggEvent() {
+  let dropzone = document.querySelectorAll(".dropzone");
+  console.log(dropzone);
+  dropzone.forEach((zone) => {
+    zone.addEventListener("dragstart", dragStart);
+    zone.addEventListener("dragover", dragOver);
+    zone.addEventListener("drop", drop);
+  });
+
+  function dragStart(e) {
+    dragged = e.target;
+    e.dataTransfer.setData("text", "");
+  }
+
+  function dragOver(e) {
+    if(e.target.className.includes("dropzone")){
+      e.preventDefault()
+    }
+  }
+
+  function drop(e) {
+    e.target.appendChild(dragged);
+  }
 }
